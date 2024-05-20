1b868b849c03858c3b622071c63423d6f679df4f
logic233 <925663486@qq.com>
2024-05-20 13:15:31 +0800

    copy from idm
    1

--- planner/__init__.py
+++ planner/__init__.py
@@ -1,6 +1,7 @@
-from planner.IDM.idm import IDM 
+# from planner.IDM.idm import IDM 
+from planner.IDM_PLUS.idm import IDM 
 # from planner.Manual.manual_controller import ManualController
 # from planner.Lattice.lattice import LATTICE
 from planner.lauch.lauch import LAUCH
 
-PLANNER = LAUCH
+PLANNER = IDM
